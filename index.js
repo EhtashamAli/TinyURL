c215a424a61c02dc1ef8ee23164b9d5996109143
EhtashamAli <ehtasham.ali1979@gmail.com>
2020-04-01 08:03:01 +0500

    📦 NEW: Shorten URL function added

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 let http = require("http");
+function extractHostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+
+  if (url.indexOf("//") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(":")[0];
+  //find & remove "?"
+  hostname = hostname.split("?")[0];
+
+  return hostname;
+}
+async function extractRootDomain(url) {
+  var domain = extractHostname(url),
+    splitArr = domain.split("."),
+    arrLen = splitArr.length;
+
+  //extracting the root domain here
+  //if there is a subdomain
+  if (arrLen > 2) {
+    domain = splitArr[arrLen - 2] + "." + splitArr[arrLen - 1];
+    //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+    if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+      //this is using a ccTLD
+      domain = splitArr[arrLen - 3] + "." + domain;
+    }
+  }
+  //change . to _
+  domain = domain.replace(".", "_");
+  return domain;
+}
 
 module.exports = {
   shorten: async url =>
@@ -30,5 +66,7 @@ module.exports = {
           resp.on("end", () => _resolve(rawData));
         })
         .on("error", error => _reject(error.message))
-    )
+    ),
+  extractRootDomain,
+  extractHostname
 };
