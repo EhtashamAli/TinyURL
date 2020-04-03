eb3d6e1d0d223ba3752271b4dac94df11aae65e6
EhtashamAli <ehtasham.ali1979@gmail.com>
2020-04-03 11:59:39 +0500

    📦 NEW: mocha/nyc

--- spec/unit/validateInput.spec.js
+++ spec/unit/validateInput.spec.js
@@ -1,5 +1,6 @@
 let { validateInput } = require("../../util/util");
 let should = require("should");
+var assert = require("assert");
 describe("validateInput tests ", function() {
   var _url;
   var _alias;
@@ -8,20 +9,20 @@ describe("validateInput tests ", function() {
     _url = "https://facebook.com";
     _alias = "asdasdasda";
     var result = validateInput({ url: _url, alias: _alias });
-    expect(result).toEqual(true);
+    result.should.equal(true);
   });
   it("should return error", function() {
     _url = "";
     _alias = "asdasdads";
-    expect(function() {
+    (function() {
       validateInput({ url: _url, alias: _alias });
-    }).toThrow(new Error("The url can not be an empty string."));
+    }.should.throw(new Error("The url can not be an empty string.")));
   });
   it("should return error", function() {
     _url = "https://facebook.com";
     _alias = "dads";
-    expect(function() {
+    (function() {
       validateInput({ url: _url, alias: _alias });
-    }).toThrow(new Error("The alias must be at least 5 characters."));
+    }.should.throw(new Error("The alias must be at least 5 characters.")));
   });
 });
